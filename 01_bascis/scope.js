25d379e948ba5e0a78c5fd92ec0466df1b5136d5
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-04-11 21:52:49 +0530

    Scope

--- 01_bascis/scope.js
+++ 01_bascis/scope.js
@@ -1,9 +1,36 @@
-let a = 10
+// let a = 10
 
-if(true){
-    const b = 20
-    console.log("Inner:",a)
+// if(true){
+//     const b = 20
+//     console.log("Inner:",a)
+// }
+
+// console.log(a)
+// console.log(b)
+
+function one(){
+    const userName = "abhi"
+
+    function two(){
+        const website = "youtube"
+        console.log(userName)
+    }
+    // console.log(website)
+    two()
+    
 }
 
-console.log(a)
-// console.log(b)
+one()
+
+
+//*************************** */
+addOne(5)
+function addOne(value){
+    return value+1
+}
+
+
+addTwo(5)
+const addTwo = function(value){  //Give the error the hoisting
+    return value+2
+}
