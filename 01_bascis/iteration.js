272b6de87a0d146fb1d832e3e15d27ba301606d7
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-04-16 19:32:27 +0530

    iteration

---
+++ 01_bascis/iteration.js
@@ -0,0 +1,35 @@
+//for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 is best number")
+//     }
+//     console.log(element)
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`Inner loop value: ${j} and outer loop value ${i}`)        
+//     }
+//     const element = i;
+    
+// }
+
+// let array = ['flash','batman', 'iron_man']
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element)
+    
+// }
+
+
+// break and continue
+for (let i = 0; i < 10; i++) {
+    if(i == 5){
+        console.log(i);
+        break
+    }
+    
+}
